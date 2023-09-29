857e602d43e780c234ec0a11e38e6d5c5a454cd5
Daniel Cabasa <cabasadaniel1@gmail.com>
2023-09-29 21:37:39 +0800

    Some Changes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $users = DB::select("SELECT * FROM users");
+    dd($users);
+    // return view('welcome');
 });
 
 Route::get('/dashboard', function () {
